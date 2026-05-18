ebed39572394811ab9c92f2d4f51ba0585f9f1fa
wz <wenzhe1423@gmail.com>
2026-05-18 13:35:00 +0800

    arrays and arrayList

--- lecturew3.java
+++ lecturew3.java
@@ -1,14 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class lecturew3 {
 
     public static void main(String[] args) {
-        System.out.println(args[0]);
-        int num1 = 2;
-        int num2 = 5;
-        System.out.println("Result is " + add2Numbers(num1, num2));
+        // Normal Array for Primitives and Objects, Can Be initialised with {}
+        int[] numbers = new int[] {1, 2};
+
+        // Dynamic list, can be expanded, requires objects as the variable type
+        ArrayList<Integer> specialNumbers = new ArrayList<>(
+            Arrays.asList(1, 2, 3)
+        );
+
+        specialNumbers.add(10);
+
+        for (Integer number : specialNumbers) {
+            System.out.println(number);
+        }
+        System.out.println("Result is " + add2Numbers(numbers[0], numbers[1]));
     }
 
     public static int add2Numbers(int num1, int num2) {
-        int result = num1 * num2;
         return num1 + num2;
     }
 }
