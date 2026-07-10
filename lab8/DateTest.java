b275895cb649507b760a12af42e38435d003abbd
wz-Tan <wenzhe1423@gmail.com>
2026-07-10 17:28:35 +0800

    test git setup

--- lab8/DateTest.java
+++ lab8/DateTest.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class DateTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] arg) {
         Scanner scanner = new Scanner(System.in);
         int day;
         int month;
